3d943b0a9c873ee4a744c249d4e07ad3174d5a57
lbrasseur <lbrasseur@4bdd3659-77f4-8c2c-b96e-5bfcaddf7b40>
2010-07-21 03:47:29 +0000

    First commit

---
+++ trunk/ar.com.oxen.android.sample/src/main/java/ar/com/oxen/android/sample/SampleConfigModule.java
@@ -0,0 +1,21 @@
+package ar.com.oxen.android.sample;
+
+import roboguice.config.AbstractAndroidModule;
+
+
+
+/**
+ * Sample configuration module
+ * 
+ * @author lbrasseur
+ * 
+ */
+public class SampleConfigModule extends AbstractAndroidModule {
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void configure() {
+		
+	}
+}
