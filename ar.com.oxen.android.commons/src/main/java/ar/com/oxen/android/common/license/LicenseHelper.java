ac515702154cbbb9b3e06d4e42999920d290404d
lbrasseur@gmail.com <lbrasseur@gmail.com@4bdd3659-77f4-8c2c-b96e-5bfcaddf7b40>
2013-01-03 22:22:59 +0000

    license helper adjustments

--- ar.com.oxen.android.commons/src/main/java/ar/com/oxen/android/common/license/LicenseHelper.java
+++ ar.com.oxen.android.commons/src/main/java/ar/com/oxen/android/common/license/LicenseHelper.java
@@ -11,47 +11,45 @@ import java.io.Serializable;
 import javax.inject.Inject;
 
 import android.content.Context;
-import ar.com.oxen.commons.license.api.HardwareIdProvider;
 import ar.com.oxen.commons.license.api.License;
 import ar.com.oxen.commons.license.api.LicenseSerializer;
 import ar.com.oxen.commons.license.api.LicenseValidator;
-import ar.com.oxen.commons.license.impl.DefaultLicenseInfo;
 
 public class LicenseHelper<I extends Serializable> {
 	private final static String LICENSE_FILE = "license.txt";
 	private Context context;
 	private LicenseSerializer<I> licenseSerializer;
 	private LicenseValidator<I> licenseValidator;
-	private HardwareIdProvider hardwareIdProvider;
 
 	@Inject
 	public LicenseHelper(Context context,
 			LicenseSerializer<I> licenseSerializer,
-			LicenseValidator<I> licenseValidator,
-			HardwareIdProvider hardwareIdProvider) {
+			LicenseValidator<I> licenseValidator) {
 		super();
 		this.context = context;
 		this.licenseSerializer = licenseSerializer;
 		this.licenseValidator = licenseValidator;
-		this.hardwareIdProvider = hardwareIdProvider;
 	}
 
-	public void runLicensed(Runnable runnable) {
-		boolean valid = false;
-
+	public boolean isLicenseOk() {
 		try {
-			valid = this.licenseValidator.validate(this.getLicense());
+			return this.licenseValidator.validate(this.readLicense());
 		} catch (Exception e) {
-		}
-		
-		if (valid) {
-			runnable.run();
-		} else {
-			borrame();
+			return false;
 		}
 	}
 
-	private License<I> getLicense() {
+	public License<I> readLicense() {
+		String licenseString = this.readLicenseString();
+
+		if (licenseString != null && !licenseString.trim().equals("")) {
+			return this.licenseSerializer.deserializeLicence(licenseString);
+		} else {
+			return null;
+		}
+	}
+
+	public String readLicenseString() {
 		String licenseString = null;
 		BufferedReader reader = null;
 
@@ -69,25 +67,22 @@ public class LicenseHelper<I extends Serializable> {
 			}
 		}
 
-		if (licenseString != null && !licenseString.trim().equals("")) {
-			return this.licenseSerializer.deserializeLicence(licenseString);
-		} else {
-			return null;
-		}
+		return licenseString;
 	}
 
-	private void borrame() {
+	public void saveLicense(License<I> license) {
+		this.saveLicenseString(this.licenseSerializer.serializeLicence(license));
+	}
+
+	public void saveLicenseString(String licenseString) {
 		PrintWriter writer = null;
 		try {
 			writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(
-					this.context.openFileOutput("requerimiento.txt",
+					this.context.openFileOutput(LICENSE_FILE,
 							Context.MODE_WORLD_WRITEABLE))));
-			writer.println(this.licenseSerializer
-					.serializeLicenceInfo((I) new DefaultLicenseInfo("Apple","mi modulo",
-							null, "mi codigo", hardwareIdProvider
-									.getHardwareId())));
+			writer.println(licenseString);
 		} catch (Exception e) {
-			e.printStackTrace();
+			throw new IllegalStateException(e);
 		} finally {
 			if (writer != null) {
 				writer.close();
